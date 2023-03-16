bdb173b7e03ef391692ab573cc04779cef88de73
ShaelinN <shaelin100@gmail.com>
2023-03-16 22:15:47 +0200

    Minor changes for Docker containerization

--- Back/JavaSpring/guitartuner/src/main/java/com/shaelinn/guitartuner/services/GuitarTuningDBConfig.java
+++ Back/JavaSpring/guitartuner/src/main/java/com/shaelinn/guitartuner/services/GuitarTuningDBConfig.java
@@ -4,9 +4,14 @@ import org.hibernate.cfg.Configuration;
 
 public class GuitarTuningDBConfig extends Configuration {
     public GuitarTuningDBConfig() {
-        this.setProperty("hibernate.connection.url", System.getProperty("GUITAR_TUNER_DB_URL"));
-        this.setProperty("hibernate.connection.username", System.getProperty("GUITAR_TUNER_DB_USER"));
-        this.setProperty("hibernate.connection.password", System.getProperty("GUITAR_TUNER_DB_PASSWORD"));
+        String url = "jdbc:mysql://host.docker.internal:3306/" + System.getenv("DB_DATABASE");
+        String pass =  System.getenv("DB_PASSWORD");
+        String user = System.getenv("DB_USERNAME");
+
+        this.setProperty("hibernate.connection.url", url);
+        this.setProperty("hibernate.connection.username", user);
+        this.setProperty("hibernate.connection.password", pass);
+
         this.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
         this.setProperty("hibernate.show_sql", "true");
 
--- Scripts/createDB.sql
+++ Scripts/createDB.sql
@@ -3,15 +3,15 @@ CREATE DATABASE GuitarTunerDB;
 USE GuitarTunerDB;
 
 Create table Tuning (
-    -> id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    -> category VARCHAR(255),
-    -> subcategory VARCHAR(255),
-    -> name VARCHAR(255),
-    -> s1 TINYINT(255),
-    -> s2 TINYINT(255),
-    -> s3 TINYINT(255),
-    -> s4 TINYINT(255),
-    -> s5 TINYINT(255),
-    -> s6 TINYINT(255)
-    -> 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category VARCHAR(255),
+    subcategory VARCHAR(255),
+    name VARCHAR(255),
+    s1 TINYINT(255),
+    s2 TINYINT(255),
+    s3 TINYINT(255),
+    s4 TINYINT(255),
+    s5 TINYINT(255),
+    s6 TINYINT(255)
+    
 );
